941344150808fde2221649c2ce960c5241e08d39
mmlumba <mmlumba@gmail.com>
2015-09-24 22:33:50 -0400

    update

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
 	twitter: {
-  	consumer_key: 'QhuXAxxU3XnMeC7asOKGoe3DX',
-		consumer_secret: 'czbm5hqDUuaWqg8fl8ZOzZzIENgOPP6rig0Y4MocVuAn1o6gTK',
-		access_token: '2240284736-9xsc3hF5KrjyYWT5Jv0UaApFOPA2e9gMBl0ZXex',
-		access_token_secret: 'CdOhxvrrsN9LaBkXLt5LE6fTiftrtbu7wUM1LLjKqieb3'
+  	consumer_key: 'hJ75vlDtueJeRauUXu5ow',
+		consumer_secret: '0lh6Lejgp8C9h2ZG2TvsAn2YkiRVESMIMHFefI3sE',
+		access_token: '14255934-et8BZO1mIvU0IrCfFUMX3dOAe5POOvoAyMpOlXEZg',
+		access_token_secret: 'omsato3lgDz2zMMD2zHTiqkSwyPw3Llp04jwX26wSr4JH'
 	}
 
 };
