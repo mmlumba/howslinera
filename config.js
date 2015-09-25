5ac6e9e814698587a7f35e5dba2a6cf43d70ddfa
mmlumba <mmlumba@gmail.com>
2015-09-24 22:29:58 -0400

    config

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'QhuXAxxU3XnMeC7asOKGoe3DX',
+		consumer_secret: 'czbm5hqDUuaWqg8fl8ZOzZzIENgOPP6rig0Y4MocVuAn1o6gTK',
+		access_token: '2240284736-9xsc3hF5KrjyYWT5Jv0UaApFOPA2e9gMBl0ZXex',
+		access_token_secret: 'CdOhxvrrsN9LaBkXLt5LE6fTiftrtbu7wUM1LLjKqieb3'
+	}
+
+};
